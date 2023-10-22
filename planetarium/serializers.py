5702a254bfb2a696334c633f41d9df416f05f16d
Kristian Yendrek <kristianendreck4@gmail.com>
2023-10-22 19:33:42 +0300

    updated views & serializers for AstronomyShow, ShowSession

--- planetarium/serializers.py
+++ planetarium/serializers.py
@@ -1,6 +1,13 @@
 from rest_framework import serializers
 
-from planetarium.models import ShowTheme, AstronomyShow, PlanetariumDome, ShowSession, Reservation, Ticket
+from planetarium.models import (
+    ShowTheme,
+    AstronomyShow,
+    PlanetariumDome,
+    ShowSession,
+    Reservation,
+    Ticket,
+)
 
 
 class ShowThemeSerializer(serializers.ModelSerializer):
@@ -15,16 +22,74 @@ class AstronomyShowSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
+class AstronomyShowListSerializer(AstronomyShowSerializer):
+    show_theme = serializers.SlugRelatedField(
+        many=True, read_only=True, slug_field="name"
+    )
+
+
+class AstronomyShowDetailSerializer(AstronomyShowSerializer):
+    show_theme = ShowThemeSerializer(many=True, read_only=True)
+
+
 class PlanetariumDomeSerializer(serializers.ModelSerializer):
     class Meta:
         model = PlanetariumDome
-        fields = "__all__"
+        fields = ("id", "name", "rows", "seats_in_row", "capacity")
 
 
 class ShowSessionSerializer(serializers.ModelSerializer):
     class Meta:
         model = ShowSession
         fields = "__all__"
+
+
+class ShowSessionListSerializer(serializers.ModelSerializer):
+    astronomy_show_title = serializers.CharField(
+        source="astronomy_show.title", read_only=True
+    )
+    planetarium_dome_name = serializers.CharField(
+        source="planetarium_dome.name", read_only=True
+    )
+    planetarium_dome_capacity = serializers.IntegerField(
+        source="planetarium_dome.capacity", read_only=True
+    )
+    tickets_available = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        model = ShowSession
+        fields = (
+            "id",
+            "show_time",
+            "astronomy_show_title",
+            "planetarium_dome_name",
+            "planetarium_dome_capacity",
+            "tickets_available",
+        )
+
+
+class ShowSessionTicketSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ticket
+        fields = ("row", "seat")
+
+
+class ShowSessionDetailSerializer(serializers.ModelSerializer):
+    astronomy_show = AstronomyShowListSerializer(many=False, read_only=True)
+    planetarium_dome = PlanetariumDomeSerializer(many=False, read_only=True)
+    taken_places = ShowSessionTicketSerializer(
+        source="tickets", many=True, read_only=True
+    )
+
+    class Meta:
+        model = ShowSession
+        fields = (
+            "id",
+            "show_time",
+            "astronomy_show",
+            "planetarium_dome",
+            "taken_places",
+        )
 
 
 class ReservationSerializer(serializers.ModelSerializer):
--- planetarium/views.py
+++ planetarium/views.py
@@ -1,8 +1,26 @@
+from django.db.models import F, Count
 from rest_framework import viewsets
 
-from planetarium.models import ShowTheme, AstronomyShow, PlanetariumDome, ShowSession, Reservation, Ticket
-from planetarium.serializers import ShowThemeSerializer, AstronomyShowSerializer, PlanetariumDomeSerializer, \
-    ShowSessionSerializer, ReservationSerializer, TicketSerializer
+from planetarium.models import (
+    ShowTheme,
+    AstronomyShow,
+    PlanetariumDome,
+    ShowSession,
+    Reservation,
+    Ticket,
+)
+from planetarium.serializers import (
+    ShowThemeSerializer,
+    AstronomyShowSerializer,
+    PlanetariumDomeSerializer,
+    ShowSessionSerializer,
+    ReservationSerializer,
+    TicketSerializer,
+    AstronomyShowListSerializer,
+    AstronomyShowDetailSerializer,
+    ShowSessionListSerializer,
+    ShowSessionDetailSerializer,
+)
 
 
 class ShowThemeViewSet(viewsets.ModelViewSet):
@@ -14,6 +32,19 @@ class AstronomyShowViewSet(viewsets.ModelViewSet):
     queryset = AstronomyShow.objects.all()
     serializer_class = AstronomyShowSerializer
 
+    def get_queryset(self):
+        queryset = self.queryset.prefetch_related("show_theme")
+
+        return queryset
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return AstronomyShowListSerializer
+        if self.action == "retrieve":
+            return AstronomyShowDetailSerializer
+
+        return self.serializer_class
+
 
 class PlanetariumDomeViewSet(viewsets.ModelViewSet):
     queryset = PlanetariumDome.objects.all()
@@ -23,6 +54,31 @@ class PlanetariumDomeViewSet(viewsets.ModelViewSet):
 class ShowSessionViewSet(viewsets.ModelViewSet):
     queryset = ShowSession.objects.all()
     serializer_class = ShowSessionSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return ShowSessionListSerializer
+
+        if self.action == "retrieve":
+            return ShowSessionDetailSerializer
+
+        return self.serializer_class
+
+    def get_queryset(self):
+        queryset = self.queryset.select_related(
+            "astronomy_show", "planetarium_dome"
+        )
+
+        if self.action == "list":
+            queryset = queryset.annotate(
+                tickets_available=(
+                    F("planetarium_dome__seats_in_row")
+                    * F("planetarium_dome__rows")
+                    - Count("tickets")
+                )
+            )
+
+        return queryset.distinct()
 
 
 class ReservationViewSet(viewsets.ModelViewSet):
